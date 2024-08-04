c94d71b5976cdc8debad7dd0d22b9192b12a01d8
govind-pasupuleti <govindpasupuleti666@gmail.com>
2024-08-04 14:15:43 +0530

    added

--- test.js
+++ test.js
@@ -18,12 +18,31 @@ function create_imageContainer(item){
 
     let imageDiv=document.createElement("div");
     imageDiv.classList.add("image");
+
+
+
+   
+
     mainContainerImages.appendChild(imageDiv);
 
     let imgElement=document.createElement("img");
     imgElement.src="https://picsum.photos/200?random="+id;
     imgElement.classList.add("imgs");
-    imageDiv.appendChild(imgElement);
+
+    let anchor=document.createElement("a");
+    
+    
+    anchor.href="#sameid";
+    
+
+
+
+    anchor.target="_blank";
+
+    anchor.classList.add("btns")
+    anchor.appendChild(imgElement)
+
+    imageDiv.appendChild(anchor);
 
     let uid=document.createElement("p");
     imageDiv.appendChild(uid);
@@ -47,3 +66,47 @@ files.then(function(response){return response.json()}).then(function(responsebod
      });
      
  })
+
+
+
+ 
+
+ let specificImage=document.createElement("div");
+ specificImage.id="sameid"
+ document.body.appendChild(specificImage);
+
+
+ let divM=document.createElement("div");
+ divM.classList.add("second")
+ specificImage.appendChild(divM)
+ 
+ let h1element=document.createElement("h1");
+ h1element.textContent="Social Media App";
+ 
+ let linebreak=document.createElement("hr");
+ h1element.appendChild(linebreak);
+ 
+ let p1Element=document.createElement("p");
+ p1Element.textContent="Detials Page For Post With Id : ";
+ 
+ divM.appendChild(p1Element);
+ 
+ let imageElement=document.createElement("img");
+ imageElement.src="https://picsum.photos/200?random=";
+ imageElement.classList.add("second-image");
+ divM.appendChild(imageElement);
+ 
+ let p2Element=document.createElement("p");
+ p2Element.textContent="User Id :";
+ divM.appendChild(p2Element);
+ 
+ let p3Element=document.createElement("p");
+ p3Element.textContent="Title : ";
+ divM.appendChild(p3Element);
+ 
+ let p4Element=document.createElement("p");
+ p4Element.textContent="Body : ";
+ divM.appendChild(p4Element);
+ 
+ 
+ 
